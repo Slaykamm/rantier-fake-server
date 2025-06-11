09544100d2f33658d79f2886fe0d4422677d3562
Slaykamm <destpoch@mail.ru>
2025-06-12 00:58:13 +0300

    before typeorm

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Импортируем необходимые модули
 const express = require("express");
+const { getSumFromTransaction } = require("./utils/untils");
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,7 @@ const property = require("./mocks/property.json");
 const rent = require("./mocks/rent.json");
 const service_company = require("./mocks/service_company.json");
 const tenant = require("./mocks/tenant.json");
+const transactions = require("./mocks/transactions.json");
 
 // Middleware для обработки JSON-запросов
 app.use(express.json());
@@ -247,6 +249,51 @@ app.get("/tenant/:id", (req, res) => {
 app.post("/data", (req, res) => {
   console.log("Получены данные:", req.body);
   res.json({ status: "Данные получены", data: req.body });
+});
+
+app.post("/transactions", (req, res) => {
+  const rentId = req.body.rentId;
+
+  try {
+    const filteredByRentId = transactions?.filter(
+      (transaction) => transaction.rentId == rentId
+    );
+
+    const main = filteredByRentId.filter(
+      (invoice) => invoice.kindOfPayment == "main"
+    );
+    const mainInvoices = main.filter((invoice) => invoice.type == "invoice");
+    const mainPayments = main.filter((invoice) => invoice.type == "payment");
+
+    const service = filteredByRentId.filter(
+      (invoice) => invoice.kindOfPayment == "service"
+    );
+    const serviceInvoices = service.filter(
+      (invoice) => invoice.type == "invoice"
+    );
+    const servicePayments = service.filter(
+      (invoice) => invoice.type == "payment"
+    );
+
+    const mainInvoiceSum = getSumFromTransaction(mainInvoices);
+    const mainPaymentsSum = getSumFromTransaction(mainPayments);
+
+    const serviceInvoicesSum = getSumFromTransaction(serviceInvoices);
+    const servicePaymentsSum = getSumFromTransaction(servicePayments);
+
+    const mainTotal = mainInvoiceSum - mainPaymentsSum;
+    const serviceTotal = serviceInvoicesSum - servicePaymentsSum;
+    const total = mainTotal + serviceTotal;
+
+    return res
+      .status(200)
+      .json({ status: "success", data: [{ mainTotal, serviceTotal, total }] });
+  } catch (e) {
+    res.status(500).json({
+      status: "error",
+      message: `Transactions Service internal Error ${e}`,
+    });
+  }
 });
 
 // Запуск сервера
